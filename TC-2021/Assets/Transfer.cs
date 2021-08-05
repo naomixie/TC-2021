8dc9b5c0b77b739a6bc13972880471f3d898f46b
Naomi <41462684+naomixie@users.noreply.github.com>
2021-08-05 11:25:23 +0800

    feat: Import scene & transfer

---
+++ TC-2021/Assets/Transfer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transfer : MonoBehaviour
+{
+    public Transform where_to_go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "InitialSpawn";
+    }
+
+}
