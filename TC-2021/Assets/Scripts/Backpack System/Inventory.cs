e01fdee816c1b999712d0b305151c6f6deb69ce3
Naomi <41462684+naomixie@users.noreply.github.com>
2021-08-08 03:12:40 +0800

    feat: Small tweaks
    - Fixed the folders
    - Added gravity to objects
    - Changed the storyline
    - Upgraded the unity editor to version 2020 LTS.

---
+++ TC-2021/Assets/Scripts/Backpack System/Inventory.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int MaxCapacity = 20;
+    public int MakeOxygenTankSteelNumber = 5;
+    public int MakeOxygenTankWoodNumber = 20;
+    public int MakeLadderWood = 30;
+    public List<Item> items = new List<Item>();
+
+    public static Inventory instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public bool AddItem(Item item)
+    {
+        Debug.Log("Adding :" + item.ItemName);
+        foreach (Item it in items)
+        {
+            if (it.type == item.type)
+            {
+                it.count += item.count;
+                InventoryUI.instance.UpdateUI();
+                WarningPanel.instance.SendWarningText("Added " + item.count + " " + item.ItemName + " to inventory.");
+                return true;
+            }
+        }
+        if (items.Count < MaxCapacity)
+        {
+            items.Add(item);
+            InventoryUI.instance.UpdateUI();
+            WarningPanel.instance.SendWarningText("Added " + item.count + " " + item.ItemName + " to inventory.");
+            return true;
+        }
+        return false;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+        InventoryUI.instance.UpdateUI();
+    }
+
+    public bool MakeOxygenTank()
+    {
+        foreach (Item item in items)
+        {
+            if (item.type == "steel" && item.count >= MakeOxygenTankSteelNumber)
+            {
+                foreach (Item wood in items)
+                {
+                    if (wood.type == "wood" && wood.count >= MakeOxygenTankWoodNumber)
+                    {
+                        if (item.count - MakeOxygenTankSteelNumber == 0)
+                        {
+                            Remove(item);
+                        }
+                        else
+                        {
+                            item.count -= MakeOxygenTankSteelNumber;
+                        }
+                        if (wood.count - MakeOxygenTankWoodNumber == 0)
+                        {
+                            Remove(wood);
+                        }
+                        else
+                        {
+                            wood.count -= MakeOxygenTankWoodNumber;
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool MakeLadder()
+    {
+        foreach (Item item in items)
+        {
+            if (item.type == "wood" && item.count >= MakeLadderWood)
+            {
+                if (item.count - MakeLadderWood == 0)
+                {
+                    Remove(item);
+                }
+                else
+                {
+                    item.count -= MakeLadderWood;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool LadderExist()
+    {
+        foreach (Item item in items)
+        {
+            if (item.type == "ladder")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool DeductItem(string type, int number)
+    {
+        foreach (Item item1 in items)
+        {
+            if (item1.type == type && item1.count > number)
+            {
+                item1.count -= number;
+                return true;
+            }
+            else if (item1.type == type && item1.count == number)
+            {
+                Remove(item1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ TC-2021/Assets/Scripts/Backpack System/InventoryUI.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    public static InventoryUI instance;
+    public GameObject InventoryPanel;
+
+    public Image detailsImage;
+    public Text detailsText;
+
+    public InventorySlot[] slots;
+
+    public InventorySlot selectedSlot;
+
+    public Sprite slotIdle;
+    public Sprite slotHover;
+    public Sprite slotActive;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        ResetSlots();
+        // UpdateUI();
+        InventoryPanel.SetActive(false);
+    }
+
+    public void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < Inventory.instance.items.Count)
+            {
+                slots[i].AddItem(Inventory.instance.items[i]);
+                // Debug.Log("added item " + Inventory.instance.items[i].name + " at " + i);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+
+    }
+
+    public void OnSlotEnter(InventorySlot slot)
+    {
+        ResetSlots();
+        if (selectedSlot == null || selectedSlot != slot)
+        {
+            //Debug.Log("Hovering slot");
+            slot.Background.sprite = slotHover;
+        }
+    }
+
+
+    public void OnSlotExit(InventorySlot slot)
+    {
+        ResetSlots();
+    }
+
+    public void OnSlotSelected(InventorySlot slot)
+    {
+        selectedSlot = slot;
+        Debug.Log("1selected slot is " + slot.name);
+        ResetSlots();
+        slot.Background.sprite = slotActive;
+        int index = slot.transform.GetSiblingIndex();
+        Debug.Log("2selected slot is " + slot.name);
+
+    }
+
+    public void ResetSlots()
+    {
+        foreach (InventorySlot slot in slots)
+        {
+            if (selectedSlot != null && slot == selectedSlot)
+            {
+                continue;
+            }
+            slot.Background.sprite = slotIdle;
+        }
+    }
+
+    public void Update()
+    {
+        if (selectedSlot != null)
+        {
+            detailsImage.sprite = selectedSlot.item.detailImage;
+            detailsText.text = selectedSlot.item.description;
+        }
+    }
+
+
+    public Item getSelectedItem()
+    {
+        return selectedSlot != null ? selectedSlot.item : null;
+    }
+
+}
