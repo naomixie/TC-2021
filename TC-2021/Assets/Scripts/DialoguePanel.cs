86b191bf6575e588b001a6d94944faf166bde91a
Naomi <41462684+naomixie@users.noreply.github.com>
2021-08-13 16:17:23 +0800

    wip: DialoguePanel

---
+++ TC-2021/Assets/Scripts/DialoguePanel.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialoguePanel : MonoBehaviour
+{
+    public static DialoguePanel instance;
+    public GameObject Panel;
+    public Text DialogueText;
+    //储存中间值
+    public List<string> DisplayText;
+    private float timer;
+    private bool isPrint = false;
+    public float perCharSpeed = 1;
+
+    private string current_print_text;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Update()
+    {
+        printText();
+    }
+
+    public void DisplayDialogueText(string dialogue)
+    {
+        Panel.SetActive(true);
+        isPrint = true;
+        timer = 0;
+        current_print_text = dialogue;
+    }
+
+    public void CloseDialoguePanel()
+    {
+        Panel.SetActive(false);
+    }
+
+    void printText()
+    {
+        try
+        {
+            if (isPrint)
+            {
+                DialogueText.text = current_print_text.Substring(0, (int)(perCharSpeed * timer));//截取
+                timer += Time.deltaTime;
+            }
+        }
+        catch (System.Exception)
+        {
+            printEnd();
+        }
+    }
+
+    void printEnd()
+    {
+        isPrint = false;
+    }
+}
---
+++ TC-2021/Assets/Scripts/InspectableObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectableObject : InteractableObject
+{
+    public List<string> InspectDescription;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+
+    }
+}
